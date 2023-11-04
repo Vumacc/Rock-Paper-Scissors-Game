776181006dfcc5f147d2e00b0b8acbfff5296f6f
V7q <149380921+Vumacc@users.noreply.github.com>
2023-11-04 13:47:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+from termcolor import colored
+import random
+import time
+import sys
+
+def write(text, speed=0.037):
+  for char in text:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(speed)
+
+# Variables and Dictionaries
+choose_difficulty = colored('Would you like to play normal or hard mode? \n', 'dark_grey')
+choose_card = colored('Choose rock, paper, or scissors \n', 'dark_grey')
+win = colored('You win!', 'green')
+lose = colored('You lost!', 'red')
+Draw = colored('It is a draw!', 'yellow')
+player_deck = ['rock', 'paper', 'scissors']
+enemy_deck = ['rock', 'paper', 'scissors']
+
+# Checks to see if the player won
+def Win_rock(player_card, enemy_card):
+  if player_card == 'rock' and enemy_card == 'scissors':
+    return True
+  return False
+
+def Win_paper(player_card, enemy_card):
+  if player_card == 'paper' and enemy_card == 'rock':
+    return True
+  return False
+
+def Win_scissors(player_card, enemy_card):
+  if player_card == 'scissors' and enemy_card == 'paper':
+    return True
+  return False
+
+def draw(player_card, enemy_card):
+  if player_card == enemy_card:
+    return True
+  return False
+
+# Print title
+write(colored('ROCK PAPER SCISSORS GAME \n', 'dark_grey'))
+print()
+
+# Choose game difficulty
+write(choose_difficulty)
+game_difficulty = input('> ')
+print()
+
+# If player chose normal mode
+if game_difficulty.lower() == 'normal':
+  Normal = True  # Change normal to true
+  Hard = False   # Change hard to false
+
+if Normal == True:  # if normal == true
+  write(choose_card)
+  player_card = input('> ').lower()
+  print()
+
+  # Confirm player and opponent card draws
+  if player_card in player_deck:
+    enemy_card = random.choice(enemy_deck)
+
+    # Show what card opponent chose
+    write(colored(f'The opponent chose {enemy_card}!\n', 'dark_grey'))
+    print()
+
+    # Check if player won
+    if Win_rock(player_card, enemy_card) or Win_paper(player_card, enemy_card) or Win_scissors(player_card, enemy_card):
+      print(win)   # Prints this if they won
+    elif draw(player_card, enemy_card):
+      print(Draw)  # Prints this if it was a draw
+    else:
+      print(lose)  # Prints this if they lost
